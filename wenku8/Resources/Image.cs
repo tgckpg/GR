687c825d526c61ad7280eaf2369b56a3efd23997
斟酌 鵬兄 <tgckpg@gmail.com>
2017-02-02 17:20:09 +0800

    BadgeTile functions

--- wenku8/Resources/Image.cs
+++ wenku8/Resources/Image.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
+using Microsoft.Graphics.Canvas.Geometry;
+using Microsoft.Graphics.Canvas.Text;
+
+using System;
 using System.IO;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.Graphics.Imaging;
 using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.Storage;
@@ -13,20 +22,19 @@ using Net.Astropenguin.Messaging;
 
 namespace wenku8.Resources
 {
-    using global::System.Runtime.InteropServices.WindowsRuntime;
     using Model.Book;
     using Settings;
     using System;
 
     static class Image
-	{
-		public static byte[] EMPTY_IMAGE = new byte[]{
-			71, 73, 70, 56, 57, 97, 1, 0, 1
-			, 0, 128, 0, 0, 255, 255, 255
-			, 0, 0, 0, 33, 249, 4, 1, 0, 0
-			, 0, 0, 44, 0, 0, 0, 0, 1, 0, 1
-			, 0, 0, 2, 2, 68, 1, 0, 59
-		};
+    {
+        public static byte[] EMPTY_IMAGE = new byte[]{
+            71, 73, 70, 56, 57, 97, 1, 0, 1
+            , 0, 128, 0, 0, 255, 255, 255
+            , 0, 0, 0, 33, 249, 4, 1, 0, 0
+            , 0, 0, 44, 0, 0, 0, 0, 1, 0, 1
+            , 0, 0, 2, 2, 68, 1, 0, 59
+        };
 
         public static async Task<BitmapImage> NewBitmap( Uri Uri = null )
         {
@@ -118,6 +126,79 @@ namespace wenku8.Resources
 
             ReturnAppStoragePath:
             return "ms-appdata:///local/" + TilePath;
+        }
+
+        public static IDisposable CreateCanvasDevice() { return new CanvasDevice(); }
+
+        public static async Task<string> LiveTileBadgeImage( IDisposable CDevice, BookItem Book, float Width, float Height, string Text )
+        {
+            if ( !( CDevice is CanvasDevice ) ) throw new ArgumentException( "Not a CanvasDevice" );
+            CanvasDevice dev = ( CanvasDevice ) CDevice;
+
+            try
+            {
+                using ( CanvasRenderTarget RenderTarget = new CanvasRenderTarget( dev, Width, Height, 96 ) )
+                {
+                    using ( CanvasDrawingSession ds = RenderTarget.CreateDrawingSession() )
+                    {
+                        if ( Shared.Storage.FileExists( Book.CoverPath ) )
+                        {
+                            using ( Stream s = Shared.Storage.GetStream( Book.CoverPath ) )
+                                await PunchImage( dev, ds, s, Width, Height, Text );
+                        }
+                        else
+                        {
+                            using ( Stream s = ( await AppStorage.AppXGetBytes( "/Assets/Samples/Empty150.png" ) ).AsBuffer().AsStream() )
+                                await PunchImage( dev, ds, s, Width, Height, Text );
+                        }
+                    }
+
+                    string FileName = FileLinks.ROOT_TILE + Book.Id + string.Format( ".{0}x{1}.tile", ( int ) Width, ( int ) Height );
+                    await RenderTarget.SaveAsync( Path.Combine( ApplicationData.Current.LocalFolder.Path, FileName ), CanvasBitmapFileFormat.Png );
+                    return "ms-appdata:///local/" + FileName;
+                }
+            }
+            catch ( Exception ex )
+            {
+                global::System.Diagnostics.Debugger.Break();
+            }
+
+            return null;
+        }
+
+        private static async Task PunchImage( CanvasDevice dev, CanvasDrawingSession ds, Stream ImageStream, float Width, float Height, string Text )
+        {
+            CanvasTextFormat Format = new CanvasTextFormat()
+            {
+                FontSize = 12
+                , VerticalAlignment = CanvasVerticalAlignment.Top
+                , HorizontalAlignment = CanvasHorizontalAlignment.Left
+                , FontFamily = "Segoe MDL2 Assets"
+            };
+
+            CanvasBitmap Bitmap = await CanvasBitmap.LoadAsync( dev, ImageStream.AsRandomAccessStream() );
+
+            CanvasCommandList ScaledBitmap = new CanvasCommandList( ds );
+            uint FillSize = Math.Min( Bitmap.SizeInPixels.Width, Bitmap.SizeInPixels.Height );
+
+            using ( CanvasDrawingSession sds = ScaledBitmap.CreateDrawingSession() )
+            {
+                sds.DrawImage( Bitmap, new Rect( 0, 0, Width, Height ), new Rect( 0, 0, FillSize, FillSize ) );
+            }
+
+            CanvasGeometry ImageRect = CanvasGeometry.CreateRectangle( dev, 0, 0, Width, Height );
+            CanvasGeometry Badge = CanvasGeometry.CreateCircle( dev, new Vector2( 15, 15 ), 10 );
+
+            CanvasGeometry Combined = ImageRect.CombineWith( Badge, Matrix3x2.CreateTranslation( 0, 0 ), CanvasGeometryCombine.Exclude );
+
+            CanvasTextLayout TextLayout = new CanvasTextLayout( dev, Text, Format, Width, Height );
+
+            CanvasImageBrush Brush = new CanvasImageBrush( ds, ScaledBitmap );
+            Brush.SourceRectangle = new Rect( 0, 0, FillSize, FillSize );
+
+            ds.FillGeometry( Combined, Brush );
+
+            ds.DrawTextLayout( TextLayout, 9f, 9f, Colors.White );
         }
 
         public static async void Destroy( ImageSource Source )
