613bd74bd56e9c0fa266beceba34df767c7a3227
斟酌 鵬兄 <tgckpg@gmail.com>
2017-12-02 22:33:33 +0800

    Removed ReadingAnchors handling

--- wenku8/Storage/AnchorStorage.cs
+++ wenku8/Storage/AnchorStorage.cs
@@ -10,7 +10,6 @@ using Net.Astropenguin.Logging;
 
 namespace wenku8.Storage
 {
-	using Resources;
 	using Model.Book;
 	using Settings;
 
@@ -80,13 +79,11 @@ namespace wenku8.Storage
 		public static readonly string ID = typeof( AutoAnchor ).Name;
 
 		private string VOL_ANC;
-		private OAutoAnchor OAnchors;
 
 		public AutoAnchor( BookItem b )
 			: base( b )
 		{
 			VOL_ANC = ( b.IsDeathblow() ? "ALT.D." : "" ) + AppKeys.LBS_CH;
-			OAnchors = new OAutoAnchor();
 		}
 
 		public void SaveAutoVolAnc( string cid )
@@ -123,16 +120,7 @@ namespace wenku8.Storage
 		{
 			XParameter XParam = Reg.Parameter( VOL_ANC );
 
-			if( XParam == null )
-			{
-				string cid = OAnchors.GetBookmark( aid );
-				if( !string.IsNullOrEmpty( cid ) )
-				{
-					SaveAutoVolAnc( cid );
-					return cid;
-				}
-			}
-			else
+			if( XParam != null )
 			{
 				return XParam.GetValue( AppKeys.GLOBAL_CID );
 			}
@@ -144,16 +132,7 @@ namespace wenku8.Storage
 		{
 			XParameter XParam = Reg.Parameter( "CAnc:" + cid );
 
-			if( XParam == null )
-			{
-				int OReadAnchor = OAnchors.GetReadAnchor( cid );
-				if( 0 < OReadAnchor )
-				{
-					SaveAutoChAnc( cid, OReadAnchor );
-					return OReadAnchor;
-				}
-			}
-			else
+			if( XParam != null )
 			{
 				return XParam.GetSaveInt( AppKeys.LBS_INDEX );
 			}
@@ -163,46 +142,7 @@ namespace wenku8.Storage
 
 		public override async Task SyncSettings()
 		{
-			await OAnchors.SyncSettings();
 			await base.SyncSettings();
 		}
-	}
-
-	[Obsolete( "ReadingAnchors.xml will be removed after Dec 2017" )]
-	sealed class OAutoAnchor
-	{
-		private const string RFileName = FileLinks.ROOT_SETTING + FileLinks.READING_ANCHORS;
-
-		private XRegistry WBookAnchors;
-
-		public OAutoAnchor()
-		{
-			WBookAnchors = new XRegistry( AppKeys.LBS_AXML, RFileName );
-		}
-
-		public string GetBookmark( string aid )
-		{
-			XParameter p = WBookAnchors.Parameter( aid );
-			if ( p != null )
-				return p.GetValue( AppKeys.GLOBAL_CID );
-			return null;
-		}
-
-		public int GetReadAnchor( string cid )
-		{
-			XParameter p = WBookAnchors.Parameter( cid );
-			if( p!= null )
-				return int.Parse( p.GetValue( AppKeys.LBS_INDEX ) );
-			return 0;
-		}
-
-		public async Task SyncSettings()
-		{
-			if ( !Shared.Storage.FileExists( RFileName ) )
-			{
-				await OneDriveSync.Instance.SyncRegistry( WBookAnchors, OneDriveSync.SyncMode.FAVOR_REMOTE );
-			}
-		}
-
 	}
 }
